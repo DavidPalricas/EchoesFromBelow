6620f6468234fefb37a74d091ebfac003f667444
davidpalricas <davipalricas22@gmail.com>
2024-10-27 23:48:53 +0000

    Enemy Movement Update (enemies can avoid obstacles and chase the player)

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -6,7 +8,7 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     /// <summary>
-    /// The plyers variable is responsible for storing the player's Rigidbody2D component.
+    /// The player variable is responsible for storing the player's Rigidbody2D component.
     /// </summary>
     private Rigidbody2D player;
 
@@ -17,101 +19,155 @@ public class EnemyMovement : MonoBehaviour
 
     /// <summary>
     /// The speed variable is responsible for storing the enemy's speed.
-    /// The SerializeField attribute is used to show the variable in the Unity Inspector
     /// </summary>
-    [SerializeField] float speed;
+    private float speed;
 
     /// <summary>
-    /// The Awake method is called when the script instance is being loaded(Unity Method).
-    /// In this method, we are getting the player's and the enemy Rigidbody2D components and checking the enemy's spawn direction.
+    /// The willCollide variable is responsible for storing whether the enemy will collide with an obstacle.
+    /// </summary>
+    private bool willCollide;
+
+    /// <summary>
+    /// The movetoNotCollide variable is responsible for storing the direction the enemy will move to avoid colliding with an obstacle.
+    /// </summary>
+    private Vector2 movetoNotCollide;
+
+    /// <summary>
+    /// The attemptedDirections variable is responsible for storing the directions that have already been attempted.
+    /// </summary>
+    private readonly HashSet<Vector2> attemptedDirections = new();
+
+
+    /// <summary>
+    /// The Awake method is called when the script instance is being loaded (Unity Method).
+    /// In this method, the enemy,speed,player and willCollide variables are initialized.
     /// </summary>
     private void Awake()
     {
-        player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
         enemy = GetComponent<Rigidbody2D>();
+        speed = 4f;
 
-
+        player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        willCollide = false;
     }
 
     /// <summary>
-    /// The Update method is called every frame(Unity Method).
-    /// In this method, we are moving the enemy to chase the player.
-    /// It also checks if the enemy is on the right or left side of the player,and sets the enemy's default movement.
+    /// The Update method is called every frame (Unity Method).
+    /// In this method, the direction to the player is calculated and the enemy's movement is handled.
+    /// If the enemy will collide, the HandleCollision method is called, otherwise the ChasePlayer method is called.
     /// </summary>
     private void Update()
     {
-        bool isEnemyRight = enemy.position.x > player.position.x;
-        var defaultMove = isEnemyRight ? new Vector2(-speed, enemy.velocity.y) : new Vector2(speed, enemy.velocity.y);
+        Vector2 directionToPlayer = (player.position - enemy.position).normalized;
 
-        ChasePlayerHorizontaly(isEnemyRight,defaultMove);
-
-        ChasePlayerVertically(defaultMove);
-    }
-
-    /// <summary>
-    /// The ChasePlayerHorizontaly method is responsible for moving the enemy to chase the player horizontaly.
-    /// </summary>
-    /// <param name="isEnemyRight">The isEnemyRight variable indicates if the enemy is on the right side of the player .</param>
-    /// <param name="defaultMove">The defaultMove variable stores the default move of the enemy.</param>
-    private void ChasePlayerHorizontaly(bool isEnemyRight,Vector2 defaultMove)
-    {
-        // If the player is moving left and the enemy is on the right side of the player, the enemy will move left.
-        // If the player is moving right the enemy wont chase the player.
-        if (player.velocity.x < 0 && isEnemyRight)
-        {
-            enemy.velocity = new Vector2(-speed, enemy.velocity.y);
+        if (willCollide)
+        {   
+            HandleCollision(directionToPlayer);   
         }
-        // If the player is moving right and the enemy is on the left side of the player, the enemy will move right.
-        // If the player is moving left the enemy wont chase the player.
-        else if (player.velocity.x > 0 && !isEnemyRight)
-        {
-            enemy.velocity = new Vector2(speed, enemy.velocity.y);
-        }
-
-        // If the player is not moving horizontally, the enemy will move in its default direction.
-       
-        if (player.velocity.x == 0)
-        {
-            enemy.velocity = defaultMove;
-        }
-    }
-
-    /// <summary>
-    /// The ChasePlayerVertically method is responsible for moving the enemy to chase the player vertically.
-    /// </summary>
-    /// <param name="defaultMove">The defaultMove variable stores the default move of the enemy.</param>
-    private void ChasePlayerVertically(Vector2 defaultMove)
-    {
-        // If the player is moving up, the enemy will move up.
-        if (player.velocity.y < 0)
-        {
-            enemy.velocity = new Vector2(enemy.velocity.x, -speed);
-        }
-        // If the player is moving down, the enemy will move down.
-        else if (player.velocity.y > 0)
-        {
-            enemy.velocity = new Vector2(enemy.velocity.x, speed);
-        }
-        // If the player is not moving vertically, the enemy will move in its default direction.
         else
         {
-            enemy.velocity = defaultMove;
+          ChasePlayer(directionToPlayer);
+        }
+    }
+
+
+    /// <summary>
+    /// The HandleCollision method is responsible for handling the enemy's movement when it will collide with an obstacle.
+    /// If the path is clear, the enemy's velocity is set to the direction it should move.
+    /// </summary>
+    /// <param name="directionToPlayer">The directionToPlayer parameter stores an vector which represents te distance of the enemy to the player</param>
+    private void HandleCollision(Vector2 directionToPlayer)
+    {
+        enemy.velocity = movetoNotCollide * speed;
+
+        if (IsPathClear(directionToPlayer))
+        {
+            willCollide = false;
+            attemptedDirections.Clear(); 
         }
     }
 
     /// <summary>
-    /// The OnCollisionEnter2D method is called when a collider  has begun touching the collider of the enemy (Unity Method).
-    /// In this method, we are ignoring the collision between the enemy and the player. 
+    /// The ChasePlayer method is responsible for handling the enemy's movement when it will not collide with an obstacle.
+    /// If the path is clear, the enemy's velocity is set to the direction it should move.
+    /// Otherwise, the FindAlternativeDirection method is called, to find an alternative direction to move.
     /// </summary>
-    /// <param name="collision">The collision variable stores the collider of the game object that collided with the enemy.</param>
+    /// <param name="directionToPlayer">The directionToPlayer parameter stores an vector which represents te distance of the enemy to the player</param>
+    private void ChasePlayer(Vector2 directionToPlayer)
+    {
+        if (IsPathClear(directionToPlayer))
+        {
+            enemy.velocity = directionToPlayer * speed;
+        }
+        else
+        {
+            Vector2 alternativeDirection = FindAlternativeDirection(directionToPlayer);
+            enemy.velocity = alternativeDirection * speed;
+            willCollide = true;
+            movetoNotCollide = alternativeDirection;
+        }
+    }
 
+    /// <summary>
+    /// The IsPathClear method is responsible for checking if the path is clear for the enemy to move.
+    /// It uses a raycast to check if there is an obstacle in the enemy's path.
+    /// </summary>
+    /// <param name="direction">The direction parameter stores an vector which represents the direction the enemy should move</param>
+    /// <returns>It returns a boolean value, true if the path is clear and false if there is an obstacle</returns>
+    private bool IsPathClear(Vector2 direction)
+    {
+        float raycastDistance = 0.9f;
+
+        LayerMask obstacleLayer = LayerMask.GetMask("Default");
+
+        RaycastHit2D hit = Physics2D.Raycast(enemy.position, direction, raycastDistance, obstacleLayer);
+
+        // This line is used to draw a ray in the scene view for debugging purposes
+        Debug.DrawRay(enemy.position, direction * raycastDistance, Color.yellow);
+
+        return hit.collider == null || !hit.collider.CompareTag("Object");
+    }
+
+
+    /// <summary>
+    /// The FindAlternativeDirection method is responsible for finding an alternative direction for the enemy to move.
+    /// This method tries to find the closest direction to the player that is not blocked by an obstacle.
+    /// If there is no alternative direction, the method returns the blocked direction.
+    /// </summary>
+    /// <param name="blockedDirection"></param>
+    /// <returns>It returns a Vector2 which represents the direction in which the enemy should move</returns>
+    private Vector2 FindAlternativeDirection(Vector2 blockedDirection)
+    {
+        List<Vector2> alternativeDirections = Entity.Directions
+            .Where(direction => direction != blockedDirection && !attemptedDirections.Contains(direction))
+            .OrderBy(direction => Vector2.Distance((Vector2)enemy.position + direction, player.position))
+            .ToList();
+
+        foreach (var direction in alternativeDirections)
+        {
+            if (IsPathClear(direction))
+            {
+                attemptedDirections.Add(direction); 
+                return direction;
+            }
+        }
+
+        attemptedDirections.Clear(); 
+        return blockedDirection;
+    }
+
+
+    /// <summary>
+    /// The OnCollisionEnter2D method is called when the enemy collides with another object (Unity Method).
+    /// In this method, the enemy ignores the collision with the player and other enemies.
+    /// </summary>
+    /// <param name="collision">The collision parameter stores a RigidBody2D or a collider of a game object which collide with the enemy</param>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
         {
             Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
         }
-
     }   
 }
 
