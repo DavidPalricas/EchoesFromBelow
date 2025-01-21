b6c4476da2d7ac5e7de905220283235d1bcc7819
simoH <hstavares@ua.pt>
2025-01-21 14:00:07 +0000

    Colliders nos barris e pedras, Sprites de Boss acabados, Iluminação e Sombra na estátua

---
+++ Assets/Scripts/Other/layerEstatua.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class layerEstatua : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (player.transform.position.y < this.transform.position.y)
+        {
+            this.GetComponent<SpriteRenderer>().sortingOrder = 2;
+        }
+        else if (player.transform.position.y > this.transform.position.y)
+        {
+            this.GetComponent<SpriteRenderer>().sortingOrder = 4;
+        }
+    }
+}
