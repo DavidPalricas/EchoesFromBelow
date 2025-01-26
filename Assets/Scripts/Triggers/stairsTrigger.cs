3f555ba421668d39d189b324dbbb47053515d366
davidpalricas <davipalricas22@gmail.com>
2025-01-26 15:40:40 +0000

    Fix bug in stairs

--- Assets/Scripts/Triggers/stairsTrigger.cs
+++ Assets/Scripts/Triggers/stairsTrigger.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class stairsTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
-    {
-        //collision.gameObject.GetComponent<EntityFSM>().entityProprieties.speed = 3;
-        collision.gameObject.GetComponent<PlayerMovement>().speed = 4;
+    {    
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().speed = 4;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
-    {
-        //collision.gameObject.GetComponent<EntityFSM>().entityProprieties.speed = 10;
-        collision.gameObject.GetComponent<PlayerMovement>().speed = 10;
+    {    
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<PlayerMovement>().speed = 10;
+        }
     }
 }
